dac510df03ec7e0558185f12d6cf39bad9c86c48
CB <cadeburkett@gmail.com>
2020-10-17 23:23:06 -0700

    added vars and prompts

--- Develop/script.js
+++ Develop/script.js
@@ -1,14 +1,30 @@
-// Assignment Code
+var numberic = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+var lowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var uppercase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var special = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "-", "+", "=", "{", "}", "[", "]", "<", ">", "?", "/", "~"];
+var emoji = ["😈", "☠️", "💅🏻", "👠", "🕷", "🍭"];
+
+var passwordText = document.querySelector("#password")
+
 var generateBtn = document.querySelector("#generate");
+generateBtn.addEventListener("click", function() {
 
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+alert("Please answer the following questions: OK is yes. Cancel is NO");
 
-  passwordText.value = password;
+var length = prompt("Password must be between 8-128  characters long");
+  if(length < 8 || length > 128 || isNaN(length)) {
+    alert("Please try again.")
+  } else {
+    console.log("continue")
+
+var lowChars = confirm("Would you like lowercase characters?")
+var upperChars = confirm("Would you like uppercase characters?");
+var numChars = confirm("Would you like numerical characters?");
+var specChars= confirm("Would you like Special characters?");
+var emoChars = confirm("Would you like emojis?");
+console.log(lowChars, upperChars, numChars, specChars)
+
 
 }
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+})
